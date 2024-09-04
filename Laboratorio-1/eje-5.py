75d9e612311e00fdc3a97c68ceac8bc94096ac6d
Joyboy <julio.porlles.pardo@gmail.com>
2024-09-04 15:59:45 -0500

    'a'

--- Laboratorio-1/eje-5.py
+++ Laboratorio-1/eje-5.py
@@ -3,3 +3,14 @@
 # número de barras vendidas que no son del día. Después el programa debe
 # mostrar el precio habitual de una barra de pan, el descuento que se le hace por
 # no ser fresca y el coste final total.
+
+precio = 3.49
+descuento = 0.60
+barrasVendidas = int(input("Imgrese Cantidad de barra vendidas que no son del dia: "))
+print("Precio habitual: ",precio)
+print("Descuento por no ser fresca: ",round((precio -( precio* descuento)),2))
+print("Barra vendidas: ", barrasVendidas)
+print("Coste final total: ", round (barrasVendidas *(precio* descuento),2))
+
+
+
