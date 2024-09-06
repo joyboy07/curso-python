fa9beb8aed7d7bc6dcd5c622f4a185f6d0141eeb
Joyboy <julio.porlles.pardo@gmail.com>
2024-09-06 17:07:36 -0500

    'a'

--- scraping web/script.py
+++ scraping web/script.py
@@ -1,17 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+# url = 'https://serviciosweb.osiptel.gob.pe/ConsultaSIRT/Buscar/FrmVerTarifa.aspx?pTarifa=157089'
 url = 'https://dockerlabs.es/'
 respuesta = requests.get(url)
 
 if respuesta.status_code == 200:
-	print(respuesta.text)
 	soup = BeautifulSoup(respuesta.text, 'html.parser')
-	maquinas = soup.find_all('div', onclick= True)
+	maquinas = soup.find_all('button', class_='upload')
+	print(maquinas)
 
-	for maquina in maquinas:
-		onclick_text = maquina['onclick']
-		nombre_maquina = onclick_text.split("'")[1]
-		print(nombre_maquina)
+
+	# for maquina in maquinas:
+	# 	onclick_text = maquina['onclick']
+	# 	nombre_maquina = onclick_text.split("'")[1]
+	# 	print(nombre_maquina)
 else:
 	print(f'Hubo un error al hcer la peticion {respuesta.status_code}')
