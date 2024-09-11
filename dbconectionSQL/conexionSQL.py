e6f46703c129e6273fc3f198eafb2b0018c8f952
Joyboy <julio.porlles.pardo@gmail.com>
2024-09-11 17:13:45 -0500

    a

---
+++ dbconectionSQL/conexionSQL.py
@@ -0,0 +1,43 @@
+import pymssql
+
+# Datos de conexión
+
+server = '192.168.201.212'  # Por ejemplo: 'localhost' o 'mi_servidor'
+database = 'DNCN_ComercioExt'
+username = 'sa'
+password = '$Inei2019*'
+
+try:
+    # Establecer la conexión
+    conn = pymssql.connect(server, username, password, database)
+    print("Conexión exitosa.")
+
+    # Crear un cursor para ejecutar consultas
+    cursor = conn.cursor()
+
+    # Ejecutar una consulta de ejemplo
+    cursor.execute("""
+		SELECT
+			p.name AS ProcedimientoNombre
+		FROM
+			sys.procedures AS p
+		INNER JOIN
+			sys.sql_modules AS m
+			ON p.object_id = m.object_id
+		WHERE
+			m.definition LIKE '%comextma_importacion3%'
+	""")
+
+    # Imprimir los resultados
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+except pymssql.Error as e:
+    print("Error en la conexión:", e)
+
+finally:
+    # Cerrar la conexión
+    if conn:
+        conn.close()
+        print("Conexión cerrada.")
