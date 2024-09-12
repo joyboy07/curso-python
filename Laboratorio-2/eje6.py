c84732a76c157d1d59efaa55db056aec4be832d4
Joyboy <kudamono200659@gmail.com>
2024-09-11 19:04:21 -0500

    se realizo el ejercicio 5 y 6

--- Laboratorio-2/eje6.py
+++ Laboratorio-2/eje6.py
@@ -1,2 +1,22 @@
 # Escribe un programa que pida dos números enteros y que calcule su división,
 # escribiendo si la división es exacta o no.
+
+
+n1 = int(input("Ingrese primer numero: "))
+n2 = int(input("Ingrese primer numero: "))
+
+divicion = n1/n2
+
+resto =n1%n2
+
+print("Numero 1: ", n1)
+print("Numero 2: ", n2)
+print("Divicion: ",divicion)
+
+if resto == 0:
+	print("La divicion exata")
+else:
+	print("La divicion no es exacta")
+
+
+
