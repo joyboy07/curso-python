fb922586fdb3b91f56cfe5941ede4ac7c81ba841
Joyboy <julio.porlles.pardo@gmail.com>
2024-09-13 09:43:59 -0500

    a

--- Laboratorio-2/eje3.py
+++ Laboratorio-2/eje3.py
@@ -1,7 +1,15 @@
 # Sea 𝑥 = (−1.24,0.24,−2.9,6.7,−0.56), ¿qué obtendremos al utilizar 𝑐𝑒𝑖𝑙(𝑥), 𝑓𝑖𝑥(𝑥),
 # 𝑓𝑙𝑜𝑜𝑟(𝑥) 𝑦 𝑟𝑜𝑢𝑛𝑑(𝑥)? Escriba el programa.
+import numpy as np
 
+x = np.array([-1.24, 0.24, -2.9, 6.7, -0.56])
 
+ceil_x = np.ceil(x)
+fix_x = np.fix(x)
+floor_x = np.floor(x)
+round_x = np.round(x)
+
+print("Resultado: ",ceil_x, fix_x, floor_x, round_x)
 
 
 
--- Laboratorio-2/eje4.py
+++ Laboratorio-2/eje4.py
@@ -1,3 +1,28 @@
 # Escribir el código en Python de la solución a la ecuación de segundo grado:
 # ax2 + bx + c = 0. Revise el Pseudocódigo de la página 74 del pdf de la sesión de
 # clase 04.
+
+def ecuacion(a, b, c):
+    if a == 0:
+        if b != 0:
+            x1 = -c / b
+            print(f"x1 = {x1}")
+        else:
+            if c != 0:
+                print('No solución!')
+            else:
+                print('Solución infinita')
+    else:
+        arg = b**2 - 4 * a * c
+        if arg < 0:
+            print('No solución real!')
+        else:
+            x1 = (-b + arg**(1/2)) / (2 * a)
+            x2 = (-b - arg**(1/2)) / (2 * a)
+            print(f"x1 = {x1}, x2 = {x2}")
+
+print(ecuacion(1, -3, 2))
+print(ecuacion(0, 2, -4))
+print(ecuacion(0, 0, 5))
+print(ecuacion(0, 0, 0))
+
